657b8e13dac62464043c461483a5c3dc6778fbd5
ChaseLEngel <chaselengel@gmail.com>
2017-03-01 22:25:11 -0800

    Request match count now increase.

--- checker.go
+++ checker.go
@@ -74,6 +74,16 @@ func (group Group) search(items []*rss.Item, requests []Request) {
 				fmt.Println("Match History Insert error: ", err)
 				continue
 			}
+
+			// Increment request's match count.
+			updatedRequest := new(Request)
+			updatedRequest.MatchCount = request.MatchCount + 1
+			stringID := fmt.Sprint(request.ID)
+			_, err = group.updateRequest(stringID, *updatedRequest)
+			if err != nil {
+				fmt.Println("Failed to updated match count: ", err)
+
+			}
 		}
 	}
 }
