0cb2531f8c60a80b529a6e0b48bc5a9b7a5a7b7f
ChaseLEngel <chaselengel@gmail.com>
2017-05-28 16:13:14 -0700

    Replaced tranfer prints with log.

--- handlers.go
+++ handlers.go
@@ -56,16 +56,18 @@ func transferRequest(w http.ResponseWriter, r *http.Request) {
 	for _, group := range groups {
 		requests, err := group.allRequests()
 		if err != nil {
-			fmt.Printf("Failed to get allRequests for group %v err: %v\n", group.ID, err)
+			log.Errorf("Failed to get allRequests for group %v err: %v\n", group.ID, err)
 			continue
 		}
 		for _, request := range requests {
+			log.Infof("Comparing %v with %v", filepath.Base(file.File), request.Regex)
 			if matched, err := regexp.MatchString(request.Regex, filepath.Base(file.File)); !matched || err != nil {
 				continue
 			}
+			log.Infof("Sending %v for %v", file.File, request.Name)
 			err := send(request, file.File)
 			if err != nil {
-				fmt.Println("Failed to send %v err: %v\n", file.File, err)
+				log.Errorf("Failed to send %v err: %v\n", file.File, err)
 				res = Response{Status{500, err.Error()}, nil}
 			}
 		}
